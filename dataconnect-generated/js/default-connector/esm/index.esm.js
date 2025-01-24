789973fffc3af2cd3bbc3e6507a7eddb798b4f32
Daniel <hopkins17@gmail.com>
2025-01-24 20:22:41 +0000

    Initialize Firebase configuration and add necessary files for project setup

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { , validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'geaux-academy',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,9 @@
+const { , validateArgs } = require('firebase/data-connect');
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'geaux-academy',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
