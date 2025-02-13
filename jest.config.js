d1c06f3b97d9c9cd1d1d22627a3076145f3b4b65
Daniel <hopkins17@gmail.com>
2025-02-13 22:07:27 +0000

    feat: update Jest configuration and remove deprecated Firebase type definitions; add @firebase/app-types dependency

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 /** @type {import('jest').Config} */
+// File: /jest.config.js
+// Description: Jest configuration file for the project.
+
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
